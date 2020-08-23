f422e54d2343f000ebd6286515ce8638126c5b61
unknown <harjot@devs.matrixmarketers.com>
2020-08-24 01:48:43 +0530

    signup page chnages

--- src/app/profile1/profile1.page.ts
+++ src/app/profile1/profile1.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { ComponentServiceService } from '../component-service.service';
+import { StorageService } from '../storage.service';
+import { ApiService } from '../api.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile1',
@@ -7,14 +11,74 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./profile1.page.scss'],
 })
 export class Profile1Page implements OnInit {
+  readonly:boolean=true;
+  userData:any;
+  profileForm:FormGroup;
 
-  constructor(public navctrl:NavController) { }
+  constructor(public navctrl:NavController,public api:ApiService,public component:ComponentServiceService,public storage:StorageService) { 
+    let userid=localStorage.getItem('user_id');
+      let fd=new FormData();
+      fd.append('user_id',userid);
+  this.api.post('view_profile.php',fd).subscribe((res:any)=>{
+  console.log(res);
+  this.userData=res.data;
+  this.profileForm.patchValue(res.data)
+    },err=>{
+      if(err)
+      {
+        this.component.presentAlert(err.message)
+      }
+    })
+  }
 
   ngOnInit() {
+    this.initProfileForm();
   }
 
   openSetting(){
 this.navctrl.navigateForward('setting')
+  }
+
+
+  editProfile()
+  {
+let alert= this.component.alert.create({
+  header:'Alert',
+  mode:'ios',
+  message:'Want to edit profile?',
+  buttons:[{
+    text:'No',
+    role:'close',
+  },
+{
+  text:'Yes',
+  handler:()=>{
+this.readonly=false;
+  }
+}]
+})
+alert.then((res)=>{
+  res.present();
+})
+
+  }
+
+  initProfileForm():void{
+    this.profileForm=new FormGroup({
+      email: new FormControl('', Validators.required),
+      mobile: new FormControl('', Validators.required),
+      location: new FormControl('', Validators.required),
+      pin_code: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+    
+
+      
+    })
+  }
+
+  update()
+  {
+    this.readonly=true;
   }
 
 }
